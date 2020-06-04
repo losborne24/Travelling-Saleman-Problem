ed1b5ed81348b61e84f50eac042dc01691d4b253
Leith Osborne <osborneleith@gmail.com>
2020-06-04 13:30:58 +0100

    first

---
+++ Travelling Saleman Problem/A Star.py
@@ -0,0 +1,240 @@
+import re
+import copy
+import sys
+import os.path as path
+
+
+def substring_after(s, splitter):               # substring(string to split, split text at splitter)
+    s = s.rstrip('\n')
+    return s.partition(splitter)[2]             # return text after '= '
+
+
+def read_write_file(filename):
+
+    all_lengths = ""                            # stores all lengths of paths in one line
+    city_file = open(filename, "r")             # open files
+    filename = substring_after(filename, "NEW") # create tour file
+    tour_file = open(path.abspath(path.join(__file__ ,"../../TourfileA/tourNEW" + filename)),'w')
+    for line in city_file:
+        if "NAME" in line:
+            name = substring_after(line, '= ')
+            print("NAME = " + name)
+            tour_file.write("NAME = " + name + "\n")
+        elif "SIZE" in line:
+            size = substring_after(line, '= ')
+            size = size.replace(',', '')
+            print("TOURSIZE = " + size + ",")
+            tour_file.write("TOURSIZE = " + size + "," + "\n")
+        else:
+            all_lengths += line
+    row_count = 0
+    col_count = 1
+    max_size = int(size)                            # size of distance matrix
+    all_lengths = all_lengths.replace("\n", "")     # convert all_lengths into an array of lengths
+    all_lengths = re.sub("[^0123456789,]", "", all_lengths)
+    all_lengths = all_lengths.split(",")
+    dist_matrix = [['-'] * int(size) for i in range(int(size))]
+    for x in range(0, len(all_lengths)):            # build dist_matrix
+        if col_count - row_count > max_size - 1:
+            row_count += 1
+            col_count = row_count + 1
+            max_size -= 1
+        dist_matrix[int(col_count)][int(row_count)] = all_lengths[x]
+        dist_matrix[int(row_count)][int(col_count)] = all_lengths[x]
+        col_count += 1
+    final_path = None
+    shortest_length = None
+    if int(size) > 25:                              # set maximum number of start locations to 25 or less
+        max_it = 25
+    else:
+        max_it = int(size)
+    for x in range(0, max_it):                      # for each start location, run A* search
+        print(x)
+        tsp_path = a_star(dist_matrix, size, x)
+        total_length = 0
+        for i in range(0, int(size) - 1):           # calculate total length of the new path
+            total_length += int(dist_matrix[int(tsp_path[i])][int(tsp_path[i + 1])])
+        total_length += int(dist_matrix[int(tsp_path[int(size) - 1])][int(tsp_path[0])])
+        if final_path is None:                      # if the new path length is shorter than the best path, replace it.
+            final_path = copy.deepcopy(tsp_path)
+            shortest_length = total_length
+        elif total_length < shortest_length:
+            final_path = copy.deepcopy(tsp_path)
+            shortest_length = total_length
+    print("LENGTH = " + str(shortest_length) + ",")
+    tour_file.write("LENGTH = " + str(shortest_length) + "," + "\n")
+    tsp_path_cs = ""
+    for c in final_path:                            # change nodes to 1,2,3...n (from 0,1,2...n-1)
+        val = int(c) + 1
+        tsp_path_cs = tsp_path_cs + str(val) + ","
+    tsp_path_cs = tsp_path_cs[:-1]                  # remove last ','
+    print(tsp_path_cs)
+    tour_file.write(tsp_path_cs)                    # write to file
+
+
+def prim(delete_rows, dist_matrix, size):           # use prim's algorithm to find minimum spanning tree
+    prim_matrix = copy.deepcopy(dist_matrix)
+    for c in delete_rows:                           # delete nodes already in path
+        for x in range(0, int(size)):
+            prim_matrix[int(c)][x] = 'x'
+            prim_matrix[x][int(c)] = 'x'
+    i = 0
+    while prim_matrix[i][i] != '-':                 # find start col
+        i += 1
+    for x in range(0, int(size)):                   # and delete start row
+        prim_matrix[int(i)][x] = 'x'
+    prim_recall(prim_matrix, size, i)               # run recursive part of prim's algorithm
+    mst_length = 0
+    for x in range(0, int(size)):                   # add up connections to find length of minimum spanning tree
+        for y in range(0, int(size)):
+            if prim_matrix[x][y].isdigit():
+                mst_length += int(prim_matrix[x][y])
+    return mst_length
+
+
+def prim_recall(prim_matrix, size, i):
+    lowest = None
+    for x in range(0, int(size)):                   # find lowest value in column
+        if lowest is None:
+            if prim_matrix[x][int(i)].isdigit():
+                lowest = int(prim_matrix[x][int(i)])
+                pos = x
+        elif prim_matrix[x][int(i)].isdigit():
+            if int(lowest) > int(prim_matrix[x][int(i)]):
+                lowest = int(prim_matrix[x][int(i)])
+                pos = x
+    if lowest is not None:                          # once found, cross out all values in row except the found number.
+        for x in range(0, int(size)):
+            if x != i:
+                prim_matrix[pos][x] = 'x'
+        prim_recall(prim_matrix, size, pos)         # repeat until a column has all its values crossed out.
+
+
+def a_star(dist_matrix, size,i):
+    connected_nodes_list = list()
+    connected_nodes_list.append(DijkstraBox([i], 0))     # Set up dijkstra's box, where '[i]' is the initial path and '0' is the working value
+    final_path = a_star_recall(dist_matrix, size, connected_nodes_list)     # run recursive A* algorithm
+    return final_path                                    # return path
+
+
+def a_star_recall(dist_matrix, size, path_list):
+    lowest = None
+    b = int(len(path_list))
+    for i in range(0, len(path_list)):        # find lowest working value
+        if lowest is None:
+            pos = i
+            lowest = path_list[i].get_working_val()
+        elif lowest > path_list[i].get_working_val():
+            pos = i
+            lowest = path_list[i].get_working_val()
+    nodes_connected = path_list[pos].get_nodes_connected()    # and get its path
+    del path_list[pos]                        # remove the path from the list of dijkstra boxes.
+    prim_length = int(prim(nodes_connected, dist_matrix, size)) # calculate the minimum spanning tree of the nodes not connected in the path
+    for n in range(0, int(size)):                       # for each node in the graph,
+        a_star_length = 0
+        if n not in nodes_connected:                    # if the node is not already in the path
+            for cn in range(0, len(nodes_connected) - 1):   # calculate the length of the current path
+                a_star_length += int(dist_matrix[nodes_connected[cn]][nodes_connected[cn+1]])
+            a_star_length += int(dist_matrix[nodes_connected[len(nodes_connected) - 1]][n]) # and add the new, final connection to the total
+            tot_length = prim_length + a_star_length    # add the new length of the path to the minimum spanning tree length
+            # also add the shortest two connections between the minimum spanning tree and the path.
+            tot_length += shortest_connections(nodes_connected[0], str(n), nodes_connected, dist_matrix, size)
+            # store the new entry into the dijkstra box list
+            path_list.append(DijkstraBox(copy.deepcopy(nodes_connected), tot_length))
+            path_list[len(path_list)-1].add_to_nodes_connected(n)   # and add new connection
+    tot_working_val = 0                                 # method of reducing list of possible connections
+    lowest = path_list[0].get_working_val()
+    for i in range(0, len(path_list)):        # find lowest working value and the mean value of all paths in list
+        tot_working_val += path_list[i].get_working_val()
+        if lowest > path_list[i].get_working_val():
+            lowest = path_list[i].get_working_val()
+    a = int((tot_working_val / len(path_list)) - lowest)  # a is the difference between the mean and minimum length
+    b = int(b*2)                                        # b is twice the total number of paths in the list
+    filter_val = int(lowest + (a / b))                  # filter value is set as lowest + (a/b)
+    for i in range(len(path_list)-1, -1, -1): # for each path in the list, if the length is greater than the filter value, remove the Dijkstra box from the list
+        if path_list[i].get_working_val() > filter_val:
+            del path_list[i]
+            i -= 1
+    # repeat until two nodes are not connected and brute force the last three connections for the two nodes.
+    if len(path_list[len(path_list)-1].get_nodes_connected()) < int(size) - 2:
+        return a_star_recall(dist_matrix, size, path_list)
+    else:
+        lowest = None
+        for i in range(0, len(path_list)):    # find shortest path in list of length size - 2
+            if len(path_list[i].get_nodes_connected()) == (int(size) - 2):
+                if lowest is None:
+                    pos = i
+                    lowest = path_list[i].get_working_val()
+                elif lowest > path_list[i].get_working_val():
+                    pos = i
+                    lowest = path_list[i].get_working_val()
+        new_node_list = path_list[pos].get_nodes_connected()
+        count = 0
+        for x in range(0, int(size)):                   # find two nodes not in psth
+            if x not in new_node_list:
+                if count == 0:
+                    node_one = int(x)
+                    count += 1
+                else:
+                    node_two = int(x)
+                    break
+        # calculate the two possible routes the tour can take and select the lower length as the final solution.
+        route_one = int(dist_matrix[new_node_list[0]][node_one]) + int(dist_matrix[new_node_list[len(new_node_list) - 1]][node_two])
+        route_two = int(dist_matrix[new_node_list[0]][node_two]) + int(dist_matrix[new_node_list[len(new_node_list) - 1]][node_one])
+        if route_one < route_two:
+            new_node_list.append(node_two)
+            new_node_list.append(node_one)
+        else:
+            new_node_list.append(node_one)
+            new_node_list.append(node_two)
+        return new_node_list
+
+
+def shortest_connections(start_node, last_node, new_node_list, dist_matrix, size):
+    shortest_start_length = None                        # find the shortest connections between the mst and path
+    pos = None
+    for x in range(0, int(size)):                       # for each node in graph, if not connected to the path,
+        if str(x) not in new_node_list and str(x) != str(last_node):
+            if dist_matrix[int(start_node)][x].isdigit():   # calculate the connection length between node 'x' and the start node
+                if shortest_start_length is None:           # select the node with the shortest connection
+                    shortest_start_length = dist_matrix[int(start_node)][x]
+                    pos = x
+                elif int(shortest_start_length) > int(dist_matrix[int(start_node)][x]):
+                    shortest_start_length = dist_matrix[int(start_node)][x]
+                    pos = x
+    shortest_last_length = None
+    for x in range(0, int(size)):
+        if dist_matrix[int(last_node)][x].isdigit():        # calculate the connection length between node 'x' and the last node in the path
+            if str(x) not in new_node_list and str(x) != str(pos):      # exclude the previous node found for the start connection
+                if shortest_last_length is None:
+                    shortest_last_length = dist_matrix[int(last_node)][x]
+                elif int(shortest_last_length) > int(dist_matrix[int(last_node)][x]):
+                    shortest_last_length = dist_matrix[int(last_node)][x]
+    return int(shortest_start_length) + int(shortest_last_length)   # return the combined length of the two connections
+
+
+class DijkstraBox:
+    def __init__(self, nodes_connected, working_val):
+        self.nodes_connected = nodes_connected
+        self.working_val = working_val
+
+    def get_nodes_connected(self):
+        return self.nodes_connected
+
+    def get_working_val(self):
+        return self.working_val
+
+    def add_to_nodes_connected(self, nodes_connected):      # add nodes to path
+        self.nodes_connected.append(nodes_connected)
+
+    def set_working_val(self, working_val):                 # update working value
+        self.working_val = working_val
+
+def main():
+    sys.setrecursionlimit(100000000)
+    read_write_file(path.abspath(path.join(__file__ ,"../../cityfiles/NEWAISearchfile012.txt")))
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ Travelling Saleman Problem/Simulated Annealing.py
@@ -0,0 +1,242 @@
+import random
+import re
+import math
+
+
+def substring_after(s, splitter):       # substring(string to split, split text at splitter)
+    s = s.rstrip('\n')
+    return s.partition(splitter)[2]     # return text after '= '
+
+
+def read_write_file(filename):
+    all_lengths = ""                    # stores all lengths of tours
+    city_file = open(filename, "r")     # open city file
+    filename = substring_after(filename, "NEW")     # create tour file
+    tour_file = open(r"C:\Users\leith\OneDrive\Documents\2nd Year\SM\AI Search\cdhg89\TourfileB\tourNEW" + filename, 'w')
+    for line in city_file:
+        if "NAME" in line:
+            name = substring_after(line, '= ')
+            print("NAME = " + name)
+            tour_file.write("NAME = " + name + "\n")
+        elif "SIZE" in line:
+            size = substring_after(line, '= ')
+            size = size.replace(',', '')
+            print("TOURSIZE = " + size + ",")
+            tour_file.write("TOURSIZE = " + size + "," + "\n")
+        else:
+            all_lengths += line
+    row_count = 0
+    col_count = 1
+    max_size = int(size)                        # size of distance matrix
+    all_lengths = all_lengths.replace("\n", "") # convert all_lengths into an array of lengths
+    all_lengths = re.sub("[^0123456789,]", "", all_lengths)
+    all_lengths = all_lengths.split(",")
+    dist_matrix = [['-'] * int(size) for i in range(int(size))]
+    for x in range(0, len(all_lengths)):        # build dist_matrix
+        if col_count - row_count > max_size - 1:
+            row_count += 1
+            col_count = row_count + 1
+            max_size -= 1
+        dist_matrix[int(col_count)][int(row_count)] = all_lengths[x]
+        dist_matrix[int(row_count)][int(col_count)] = all_lengths[x]
+        col_count += 1
+    temperature = list()                        # list of temperatures
+    temp_size = 8
+    curr_tour = list()
+    for i in range(0, int(size)):               # create initial tour 1,2,3...n
+        curr_tour.append(i)
+    set_temp(temp_size, size, curr_tour, dist_matrix, temperature)           # create initial temperature values
+    tsp_tour = sim_ann(temperature, size, curr_tour, dist_matrix, temp_size)  # run sim_ann algorithm
+    total_length = get_length(dist_matrix, tsp_tour, size)
+    print("LENGTH = " + str(total_length) + ",")
+    tour_file.write("LENGTH = " + str(total_length) + "," + "\n")
+    tsp_tour_cs = ""
+    for c in tsp_tour:                          # change nodes to 1,2,3...n (from 0,1,2...n-1)
+        val = int(c) + 1
+        tsp_tour_cs = tsp_tour_cs + str(val) + ","
+    tsp_tour_cs = tsp_tour_cs[:-1]              # remove last ','
+    print(tsp_tour_cs)
+    tour_file.write(tsp_tour_cs)                # write to file
+
+
+def get_length(dist_matrix, tour, size):        # get length of tour
+    total_length = 0
+    for i in range(0, int(size) - 1):
+        total_length += int(dist_matrix[int(tour[i])][int(tour[i + 1])])
+    total_length += int(dist_matrix[int(tour[int(size) - 1])][int(tour[0])])    # convert path length to cycle length
+    return total_length
+
+
+def sim_ann(temperature, size, curr_tour, dist_matrix, temp_size):
+    mini = 0.025                                # minimum temperature before exiting loop
+    count = 1
+    max_temp = 0
+    for i in temperature:                       # find max temp in list
+        if int(max_temp) < i:
+            max_temp = i
+    while float(max_temp) > float(mini):        # repeat while temperature is greater than minimum
+        t = 0                                   # inner loop cumulative temperature
+        c = 0                                   # inner loop counter
+        if max_temp == 0:
+            break
+        for i in range(0, int(size)):
+            tour_one = ran_tour_one(size, curr_tour)    # find three tours using three different algorithms.
+            tour_two = ran_tour_two(size, curr_tour)
+            tour_three = ran_tour_three(size, curr_tour)
+            curr_tour_length = get_length(dist_matrix, curr_tour, size)     # find the lengths of these tours
+            tour_one_length = get_length(dist_matrix, tour_one, size)
+            tour_two_length = get_length(dist_matrix, tour_two, size)
+            tour_three_length = get_length(dist_matrix, tour_three, size)
+            new_tour_length = tour_one_length
+            new_tour = tour_one
+            if new_tour_length > tour_two_length:       # and take shortest cycle as new tour.
+                new_tour = tour_two
+                new_tour_length = tour_two_length
+            if new_tour_length > tour_three_length:
+                new_tour = tour_three
+                new_tour_length = tour_three_length
+            if curr_tour_length >= new_tour_length:     # if new_tour is shorter than curr_tour, update curr_tour
+                curr_tour = new_tour
+            else:                                       # else use temperature to calculate if longer tour is acceptable
+                prob = math.exp((curr_tour_length - new_tour_length) / max_temp)
+                r = random.randint(1, 999) / 1000
+                if r < prob:                            # accept tour if random value is less than the probability
+                    t = t + (curr_tour_length - new_tour_length) / (math.log10(r))  # add temperature to cumulative total
+                    curr_tour = new_tour                # update tour
+                    c = c + 1                           # increment counter (used later to calculate mean temperature t)
+        if c != 0:
+            cooling_value = cooling_system(temp_size,temperature,count)                 # calculate cooling value (0,1)
+            temperature.remove(max_temp)                # replace max_temp with mean temperature multiplied by cooling value
+            temperature.append((t / c) * cooling_value)
+        else:                                           # if no worse tour gets accepted, c=0, and max_temp remains.
+            cooling_value = cooling_system(temp_size,temperature,count)
+            for i in range(0, temp_size):
+                if temperature[i] == max_temp:          # lower max_temp by multiplying it with cooling_value
+                    temperature[i] = max_temp * cooling_value
+        count += 1
+        max_temp = 0
+        for i in range(0, temp_size):                   # reset and find new max_temperature
+            if max_temp < temperature[i]:
+                max_temp = temperature[i]
+        # print(str(curr_tour_length) + " + " + str(count) + " + " + str(max_temp))
+    return curr_tour
+
+
+def cooling_system(temp_size, temperature, count):      # calculate cooling value by using variance
+    mean_temp = 0
+    square_mean_temp = 0
+    for i in range(0, temp_size):                       # sigma^2 = E[X^2]- [E[X]]^2
+        mean_temp += temperature[i]
+        square_mean_temp += (temperature[i]) ** 2
+    mean_temp = mean_temp / temp_size
+    square_mean_temp = square_mean_temp / temp_size
+    variance = square_mean_temp - (mean_temp ** 2)      # count ** 0.999 decreases max_temp compoundly
+    cooling_value = variance ** (1 / (1 - (variance ** (3/2)))) * (0.999 ** count)
+    return cooling_value
+
+
+def set_temp(temp_size,size, curr_tour,dist_matrix,temperature):
+    i=0
+    prob = 0.9
+    while i < temp_size:                                # while temperature list isn't full
+        tour_one = ran_tour_one(size, curr_tour)        # find shortest tour from three tour-finding algorithms
+        tour_two = ran_tour_two(size, curr_tour)
+        tour_three = ran_tour_three(size, curr_tour)
+        curr_tour_length = get_length(dist_matrix, curr_tour, size)
+        tour_one_length = get_length(dist_matrix, tour_one, size)
+        tour_two_length = get_length(dist_matrix, tour_two, size)
+        tour_three_length = get_length(dist_matrix, tour_three, size)
+        new_tour = tour_one
+        new_tour_length = tour_one_length
+        if new_tour_length > tour_two_length:
+            new_tour = tour_two
+            new_tour_length = tour_two_length
+        if new_tour_length > tour_three_length:
+            new_tour = tour_three
+            new_tour_length = tour_three_length
+        if curr_tour_length > new_tour_length:
+            curr_tour = new_tour
+        new_temp = abs((new_tour_length - curr_tour_length) / -(math.log10(prob)))  # calculate temperature values
+        if new_temp == 0:                               # if new_tour_length = curr_tour_length, find a different temperature
+            i -= 1
+        else:                                           # else add temperature to list
+            temperature.append(new_temp)
+        i+=1
+
+
+def ran_tour_one(size, curr_tour):                      # swap node one with node two
+    ran_one = random.randint(0, int(size) - 1)
+    ran_two = ran_one
+    tour = list()
+    while ran_one == ran_two:                           # make sure ran_one and ran_two are different nodes
+        ran_two = random.randint(0, int(size) - 1)
+    for i in range(0, int(size)):
+        if i == ran_one:
+            tour.append(curr_tour[ran_two])
+        elif i == ran_two:
+            tour.append(curr_tour[ran_one])
+        else:
+            tour.append(curr_tour[i])
+    return tour
+
+
+def ran_tour_two(size, curr_tour):                      # insert node ran_two next to node ran_one
+    ran_one = random.randint(0, int(size) - 1)
+    ran_two = ran_one
+    tour = list()
+    while ran_one == ran_two:                           # make sure ran_one and ran_two are different nodes
+        ran_two = random.randint(0, int(size) - 1)
+    if ran_one > ran_two:
+        temp = ran_one
+        ran_one = ran_two
+        ran_two = temp
+    shift = 0
+    for i in range(0, int(size)):                       # keep path the same until ran_one is reached
+        if shift == 0:
+            if i == ran_one:                            # then insert ran_two and shift remaining nodes
+                tour.append(curr_tour[ran_two])
+                shift = 1
+            else:
+                tour.append(curr_tour[i])
+        elif shift == 1:
+            if i == ran_two:
+                tour.append(curr_tour[i-1])
+                shift = 0
+            else:
+                tour.append(curr_tour[i-1])
+    return tour
+
+
+def ran_tour_three(size, curr_tour):                    # invert nodes between ran_one and ran_two
+    ran_one = random.randint(0, int(size) - 1)
+    ran_two = ran_one
+    tour = list()
+    while ran_one == ran_two:                           # make sure ran_one and ran_two are different nodes
+        ran_two = random.randint(0, int(size) - 1)
+    if ran_one > ran_two:                               # swap ran_one with ran_two if ran_one > ran_two
+        temp = ran_one
+        ran_one = ran_two
+        ran_two = temp
+    flip = 0
+    for i in range(0, int(size)):                       # keep path the same until ran_one is reached
+        if flip == 0:
+            if i == ran_one:                            # then inverse all nodes between ran_one and ran_two
+                flip = 1                                    # and leave remaining nodes in same position
+                tour.append(curr_tour[ran_two])
+            else:
+                tour.append(curr_tour[i])
+        else:
+            if i == ran_two:
+                flip = 0
+                tour.append(curr_tour[ran_one])
+            else:
+                tour.append(curr_tour[ran_two - (i - ran_one)])
+    return tour
+
+
+def main():
+    read_write_file(r"C:\Users\leith\OneDrive\Documents\2nd Year\SM\AI Search\cdhg89\cdhg89rest\NEWAISearchfile012.txt")
+
+if __name__ == '__main__':
+    main()
+
